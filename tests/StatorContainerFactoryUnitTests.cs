5dfd27571d21002f1ab1cb964d2f9f3808bba5ea
Stanislav Petrov <sv.petrov@kodep.pro>
2019-08-25 00:48:29 +0300

    generate in asmdef folder

---
+++ tests/StatorContainerFactoryUnitTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Stator.Tests
+{
+    public class StatorContainerFactoryUnitTests
+    {
+        [MenuItem("Window/Stator/Run unit tests")]
+        public static void RunTests()
+        {
+            var instance = new StatorContainerFactoryUnitTests();
+
+            var methods = instance.GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .Where(m => m.Name.StartsWith("Test_"))
+                .ToList();
+            
+            var passed = new List<string>();
+            var failed = new List<string>();
+
+            foreach (var testMethod in methods)
+            {
+                try
+                {
+                    testMethod.Invoke(instance, new object[0]);
+                    passed.Add($"<color=green>* {testMethod.Name} - OK</color>");
+                }
+                catch(Exception e)
+                {
+                    failed.Add($"<color=red>* {testMethod.Name} - FAIL</color>: {e.InnerException.ToString()}");
+                }
+            }
+
+            var message = string.Join("\n", failed.Union(passed));
+
+            if(failed.Count > 0)
+            {
+                EditorUtility.DisplayDialog("Stator unit tests", "Unit tests FAILED", "Ok");
+                Debug.LogError(message);
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Stator unit tests", "Unit tests PASS", "Ok");
+                Debug.Log(message);
+            }
+        }
+
+        public void Resolve_ShouldThrowBecouseNotRegistred()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        public void Test_Ok()
+        {
+
+        }
+
+        public void Test_Fail()
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ tests/TestContainerFactory.cs
@@ -0,0 +1,63 @@
+﻿using Stator;
+
+namespace StatorExample
+{
+    public partial class TestContainerFactory : ContainerFactory
+    {
+        public TestContainerFactory()
+        {
+            AddSingleton<Foo>();
+            AddSingleton<Bar>();
+            AddSingleton<FooBar>();
+            AddSingleton<object>(nameof(CreateFoo));
+            AddTransient<BarFoo>(nameof(CreateBarFoo));
+        }
+
+        public Foo CreateFoo()
+        {
+            return new Foo();
+        }
+
+        public BarFoo CreateBarFoo()
+        {
+            return new BarFoo();
+        }
+    }
+
+    public partial class TestFactory : ContainerFactory
+    {
+        public TestFactory()
+        {
+            AddTransient<Foo>();
+            AddTransient<Bar>();
+        }
+    }
+
+    public class Foo
+    {
+    }
+
+    public class Bar
+    {
+        public Foo Foo;
+        public Bar(Foo foo)
+        {
+            Foo = foo;
+        }
+    }
+
+    public class FooBar
+    {
+        public Foo Foo;
+        public Bar Bar;
+        public FooBar(Foo foo, Bar bar)
+        {
+            Foo = foo;
+            Bar = bar;
+        }
+    }
+
+    public class BarFoo
+    {
+    }
+}
