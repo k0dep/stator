2e59676c78df379087126072639780c36af213cb
Stanislav Petrov <sv.petrov@kodep.pro>
2019-08-25 22:09:44 +0300

    added post process

---
+++ src/Editor/StatorBuildPreprocessor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+
+namespace Stator.Editor
+{
+    public class StatorBuildPreprocessor : IPreprocessBuild
+    {
+        public int callbackOrder => int.MinValue;
+
+        public void OnPreprocessBuild(BuildTarget target, string path)
+        {
+            Debug.Log("[Stator] start refresh generated code before build process");
+            var service = new StatorRefreshService(new StatorSettings());
+            service.Clean();
+            AssetDatabase.Refresh();
+            service.Refresh();
+            AssetDatabase.Refresh();
+            Debug.Log("[Stator] finish refresh generated code");
+        }
+    }
+}
